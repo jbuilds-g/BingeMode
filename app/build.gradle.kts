13598a296e7cb489848951577fd1ff743d9f46c5
JBuilds <goldmanyanky72@gmail.com>
2026-09-22 15:46:09 -0400

    Use lifecycle-aware Compose state collection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ android {
 dependencies {
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.lifecycle.runtime.compose)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.compose.material.icons.core)
   implementation(libs.androidx.compose.material.icons.extended)
   implementation(libs.androidx.compose.material3)
