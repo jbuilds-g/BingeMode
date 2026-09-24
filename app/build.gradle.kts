d47fe5c3859051b68db6da55f98980d4e2bc2e9d
JBuilds <goldmanyanky72@gmail.com>
2026-09-24 06:52:25 -0400

    Set BingeMode application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.bingemode.fkgtwl"
+    applicationId = "com.jbuilds.bingemode"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
